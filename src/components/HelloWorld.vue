4ffb8a09b09a3749824387807e5c140b6a2f311c
Darjan Beciri <dbeciri@student.unipu.hr>
2023-01-30 21:06:26 +0100

    front end update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,65 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
+<script>
+
+export default {
+  data() {
+    return {
+      "poruka": 'Ovo je poruka',
+      "counter": 0,
+      "korisnik": "nema jos korisnika"
+    }
+  },
+
+  methods: {
+    incrementCounter() {
+      this.counter++;
+    },
+    decrementCounter() {
+      this.counter--;
+    },
+    zoviBackend() {
+      console.log("idemo zvati backend")
+      var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+
+      fetch("http://localhost:3000/korisnici/63ab5c8bee4fb22bc7e6d78e", requestOptions)
+        .then(response => response.text())
+        .then(result => {
+            console.log(result); 
+            this.korisnik=result
+          })
+        .catch(error => console.log('error', error));
+    }
+  }
+}
 </script>
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <h1 class="blue">{{ msg }}</h1>
     <h3>
+      <input type="text" v-model="poruka" id="inp"/>
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
+    <h2>{{poruka}}</h2>
+    <h2>Brojac je:{{counter}}</h2>
+    <button @click="incrementCounter">Click Me to increment!</button>
+    <button @click="decrementCounter">Click Me to decrement!</button>
+    <h2>Brojac je i ovdje:{{counter}}</h2>
+    <button @click="zoviBackend">Pozovi backend!</button>
+    <div>
+      <p>
+        {{korisnik}}
+      </p>
+    </div>
+
+
   </div>
 </template>
+
 
 <style scoped>
 h1 {
