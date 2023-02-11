6daabd232a0b343130b779b69b18f595545caebd
Darjan Beciri <dbeciri@student.unipu.hr>
2023-02-11 16:06:13 +0100

    dodan ispis korisnickih podataka uz oglas

---
+++ src/components/Oglas.vue
@@ -0,0 +1,82 @@
+<script>
+
+export default {
+  data() {
+    return {
+      "oglas": null
+    }
+  },
+
+  props: ['oglasId'],
+
+  methods: {
+    zoviBackend() {
+      console.log("idemo zvati backend")
+      var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+
+      fetch("http://localhost:3000/oglasi/" + this.oglasId, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            console.log(result); 
+            this.oglas=result
+          })
+        .catch(error => console.log('error', error));
+    }
+  },
+
+  mounted: function(){
+        this.zoviBackend()
+
+    }
+
+}
+</script>
+
+<template>
+  <div class="greetings">
+    <h1>naslov</h1>
+    <ul>
+        <div v-if="oglas!=null">
+            {{oglas._id}}
+            <h2>"naslov"{{oglas.naslov}}</h2>
+            <p> kategorija {{oglas.kategorija}} </p>  
+            <p> text {{oglas.text}} </p>  
+            <p> cijena {{oglas.cijena["$numberDecimal"]}}</p>  
+            <p> ime {{oglas.korisnikDetalji[0].ime }}</p>
+            <p> prezime {{oglas.korisnikDetalji[0].prezime }}</p>
+             <p> br_mob {{oglas.korisnikDetalji[0].br_mob }} </p>
+             <p> grad {{oglas.korisnikDetalji[0].grad }}</p>
+             <p> web_stranica <a :href="oglas.korisnikDetalji[0].web_stranica">{{oglas.korisnikDetalji[0].web_stranica}}</a> </p>
+            <p> ocijena {{oglas.ocijene}}</p>  
+        </div>
+    </ul>
+  </div>
+</template>
+
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
