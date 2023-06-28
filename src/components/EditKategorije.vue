b1ce65abacd687626ffe487a24f69defe04a588c
reprisa <dbeciri@student.unipu.hr>
2023-06-28 22:57:45 +0200

    editkategorija update redosl.

--- src/components/EditKategorije.vue
+++ src/components/EditKategorije.vue
@@ -4,8 +4,8 @@ export default {
         return {
             "kategorija": null,
             "naziv": "" ,
-            "opis": ""
-
+            "opis": "",
+            "redoslijed":""
         }
     },
     props: ['kategorijaId'],
@@ -26,13 +26,38 @@ export default {
                     this.kategorija = result
                     this.naziv = result.naziv
                     this.opis = result.opis
+                    this.redoslijed =result.redoslijed
             })
+
+        },
+        posalji_pod(){
+            console.log("salji podatke  ")
+            const updateData = {} 
+            updateData.naziv = this.naziv
+            updateData.redoslijed = this.redoslijed
+            updateData.opis = this.opis
+            var requestOptions = {
+                method: 'PUT',
+                redirect: 'follow',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(updateData)
+            }
+        console.log(updateData)
+        console.log("http://localhost:3000/kategorije/" + this.kategorija._id ,requestOptions)
+        fetch("http://localhost:3000/kategorije/" + this.kategorija._id  ,requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                console.log("kategorija promijenjena")
+                console.log(result)
+            })
+            .catch(error => console.log('error in katogorija update', error));
         }
     },
     mounted: function(){
         this.zoviBackend()
     }
 }
+
 </script>
 
 
@@ -43,19 +68,22 @@ export default {
     <form>
     <h2>
         {{  kategorija }}
-
     </h2>
     <label for="inputNaziv"/>
     <input name="inputNaziv" v-model="naziv"/>
     <h1> opis</h1>
     <p>{{opis}}</p>
     <input v-model = "opis"/>
+    <h1>Redoslijed</h1>
+    <p>{{redoslijed}}</p>
+    <label for="inputredoslijed"/>
+    <input name="inputredoslijed" v-model="redoslijed"/>
     </form> 
     <div>
-    <button @click=" posalji_pod()">posalji</button>  
-     
+    <button @click="posalji_pod()">posalji</button>  
+
     </div>
 
 
 </template>
-
+}
