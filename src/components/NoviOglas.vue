fbe4e427ea07ab12e5f0a652179a9dd23827fdb2
reprisa <dbeciri@student.unipu.hr>
2023-09-08 13:30:49 +0200

    Ovjavljivanje oglasa uspješno odrađeno

--- src/components/NoviOglas.vue
+++ src/components/NoviOglas.vue
@@ -1,7 +1,23 @@
+
 <template>
-  <div class="greetings">
+
     <h1>Stvaranje  novog oglasa!</h1>
-  </div>
+    <form>
+        <label for = "inputNaziv"> Naziv</label>
+        
+        <input name="inputNaziv" v-model = "naslov"/>
+        <h1> cijena </h1>
+    <label for = "inputCIjena">Cijena </label> 
+    <input name="inputCijena" v-model ="cijena" />      
+    <h1>opis</h1> 
+        <label for ="inputopis"/> 
+        <input name = "inputopis" v-model = "text"/>
+        <div>
+            <button @click="posalji_pod()">posalji</button>  
+        </div>
+    </form>
 </template>
+
+
 
 
